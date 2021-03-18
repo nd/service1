9cafcf5f1940ed98f87aac30fd05a90f58e6eba5
Dmitry Neverov <dmitry.neverov@jetbrains.com>
2021-03-18 17:50:15 +0100

    ++

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nd/service1
 
 go 1.16
+
+require github.com/nd/service3 v0.0.0-20210318164019-d589eea80a90
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/nd/service1/pkg"
+import (
+	"github.com/nd/service1/pkg"
+	"github.com/nd/service3"
+)
 
 func main() {
 	println(pkg.F())
+	println(service3.F())
 }
